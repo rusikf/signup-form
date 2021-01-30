6a0892cf5f7e0445e622d0b96ea55b68462903a4
Ruslan Korolev <great1672@gmail.com>
2021-01-30 17:45:23 +0200

    Add password indicator, btns colors on hover - css form done

--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -28,15 +28,24 @@ export const SignupForm:React.FC = () => {
           <label htmlFor='email'>Email</label>
           <input type='email' placeholder='Your email'/>
         </div>
+        <span className='error'>Valid email is required</span>
 
         <div className='input-group'>
           <label htmlFor='email'>Password</label>
           <input type='password' placeholder='Choose password'/>
         </div>
 
+        <div className='password-indicator'>
+          <div className='password-indicator_block'></div>
+          <div className='password-indicator_block'></div>
+          <div className='password-indicator_block'></div>
+          <div className='password-indicator_block'></div>
+        </div>
+        <span className='password-indicator-hint'>week</span>
+
+
         <div className='input-group'>
-          <label></label>
-          <input type='button' className='btn btn-primary' value='Sign up'></input>
+          <input type='button' className={ true ? 'btn btn-primary' : 'btn btn-primary_disabled' } value='Sign up'></input>
           </div>  
       </form>
     </div>
